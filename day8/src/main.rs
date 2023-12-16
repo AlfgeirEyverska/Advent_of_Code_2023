419b32ec345891fca4956e018470f39beb3dc034
Tyler Korte <tyjk97@gmail.com>
2023-12-16 17:26:50 -0500

    Solved day8 part1

---
+++ day8/src/main.rs
@@ -0,0 +1,51 @@
+use std::collections::HashMap;
+
+fn parse_line(line: &str) -> String {
+    line.chars()
+        .filter(|&x| x != '(' && x != ')' && x != ',' && x != '=')
+        .collect::<String>()
+}
+
+fn main() {
+    let contents = std::fs::read_to_string("input.txt").expect("Could not read file!");
+    let contents: Vec<_> = contents.lines().collect();
+
+    let turns: Vec<char> = contents[0].chars().collect();
+
+    let contents: Vec<_> = contents[2..].iter().map(|x| parse_line(x)).collect();
+    let contents: Vec<_> = contents
+        .iter()
+        .map(|x| x.split(" ").filter(|y| *y != "").collect::<Vec<_>>())
+        .collect();
+
+    let mappings = contents
+        .iter()
+        .map(|x| (x[0], (x[1], x[2])))
+        .collect::<HashMap<_, _>>();
+
+    for line in &mappings {
+        println!("{:?}", line);
+    }
+
+
+    let mut key = &"AAA";
+    let mut i = 0;
+    let mut counter = 0;
+    
+    while key != &"ZZZ" {
+
+        let turn = turns[i];
+        let (a, b) = mappings.get(key).expect("Unable to locate key {key}!");
+        key = match turn {
+            'L' => a,
+            'R' => b,
+            _ => panic!("Unknown turn given {}!", turns[i]),
+        };
+
+
+        counter += 1;
+        i = if i >= turns.len() - 1 { 0 } else { i + 1 };
+    }
+
+    println!("result: {:?}", counter); 
+}
