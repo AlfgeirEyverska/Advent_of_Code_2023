171ba2a1351237ba224c10de645e7dc8b6e13ce2
Tyler Korte <tylerkorte@TKs-MBP.local>
2023-12-18 17:29:14 -0500

    Solved day8

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,5 @@
 fn main() {
-    let file_path = "input.txt";
-    let contents = std::fs::read_to_string(file_path).expect("Unable to read file!");
+    let contents = std::fs::read_to_string("aaron.txt").expect("Unable to read file!");
 
     let acceptable = vec!['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     let nums = std::collections::HashMap::from([
@@ -21,13 +20,14 @@ fn main() {
         for (word, num) in &nums {
             line = line.replace(word, num);
         }
-        println!("{}", line);
+        // println!("{}", line);
         let res: Vec<char> = line.chars().filter(|x| acceptable.contains(x)).collect();
         // println!("{:?}", res);
         let cal_val = format!("{}{}", res[0], res[res.len() - 1]);
         let cal_val: u32 = cal_val.parse().unwrap();
         // println!("{}", cal_val);
         answer += cal_val;
+        println!("{}", cal_val);
     }
-    println!("{answer}");
+    println!("result: {answer}");
 }
--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use num::integer;
 
 fn parse_line(line: &str) -> String {
     line.chars()
@@ -6,13 +7,12 @@ fn parse_line(line: &str) -> String {
         .collect::<String>()
 }
 
-fn part1 (turns: Vec<char>, mappings: HashMap<&str,(&str, &str)>) {
+fn part1(turns: Vec<char>, mappings: HashMap<&str, (&str, &str)>) {
     let mut key = &"AAA";
     let mut i = 0;
     let mut counter = 0;
-    
-    while key != &"ZZZ" {
 
+    while key != &"ZZZ" {
         let turn = turns[i];
         let (a, b) = mappings.get(key).expect("Unable to locate key {key}!");
         key = match turn {
@@ -21,28 +21,59 @@ fn part1 (turns: Vec<char>, mappings: HashMap<&str,(&str, &str)>) {
             _ => panic!("Unknown turn given {}!", turns[i]),
         };
 
-
         counter += 1;
         i = if i >= turns.len() - 1 { 0 } else { i + 1 };
     }
 
-    println!("result: {:?}", counter); 
+    println!("result: {:?}", counter);
 }
 
-fn part2 (turns: Vec<char>, mappings: HashMap<&str,(&str, &str)>) {
+fn all_end_with_z(keys: &Vec<&str>) -> bool {
+    keys.len() == keys.iter().filter(|x| x.chars().nth(2) == Some('Z')).count()
+}
 
+fn part2(turns: Vec<char>, mappings: HashMap<&str, (&str, &str)>) {
+    let mut keys: Vec<_> = mappings
+        .keys()
+        .filter(|x| x.chars().nth(2).unwrap() == 'A')
+        .collect();
 
-    let paths: Vec<_> = mappings.keys().filter(|x| x.chars().get(2).unwrap() == 'A').collect();
-    println!("Paths:\n{:?}", paths);
-
-
-
-    let mut key = &"AAA";
     let mut i = 0;
-    let mut counter = 0;
-    
-    while key != &"ZZZ" {
+    let mut counter: u128 = 0;
+    let l = keys.len();
 
+    while l != keys.iter().filter(|&x| x.ends_with("Z")).count() {
+        // println!("{counter} -> {:?}", keys);
+
+        let turn = turns[i];
+        for j in 0..keys.len() {
+            let (a, b) = mappings.get(keys[j]).expect("Unable to locate key {key}!");
+            keys[j] = match turn {
+                'L' => &a,
+                'R' => &b,
+                _ => panic!("Unknown turn given {}!", turns[i]),
+            };
+            if keys[j].ends_with("Z") {
+                println!("{counter} -> {:?}", keys);
+            }
+        }
+
+        counter += 1;
+        i = (i + 1) % turns.len();
+        //i = if i >= turns.len() - 1 { 0 } else { i + 1 };
+    }
+    
+    // After 3 hours of running >:/
+    // 3609894725 -> ["VBN", "XPD", "MSL", "CFH", "MSQ", "BFH"]
+    println!("result: {:?}", counter);
+}
+
+fn min_steps_to_z(key: &str, turns: &Vec<char>, mappings: &HashMap<&str, (&str, &str)>) -> u128 {
+    let mut key = key;
+    let mut i = 0;
+    let mut counter: u128 = 0;
+
+    while !key.ends_with("Z") {
         let turn = turns[i];
         let (a, b) = mappings.get(key).expect("Unable to locate key {key}!");
         key = match turn {
@@ -51,14 +82,26 @@ fn part2 (turns: Vec<char>, mappings: HashMap<&str,(&str, &str)>) {
             _ => panic!("Unknown turn given {}!", turns[i]),
         };
 
-
         counter += 1;
         i = if i >= turns.len() - 1 { 0 } else { i + 1 };
     }
-
-    println!("result: {:?}", counter); 
+    counter
 }
 
+fn part2b(turns: Vec<char>, mappings: HashMap<&str, (&str, &str)>) {
+    let keys: Vec<_> = mappings
+        .keys()
+        .filter(|x| x.chars().nth(2).unwrap() == 'A')
+        .collect();
+
+    let keys: Vec<_> = keys.iter().map(|x| min_steps_to_z(x, &turns, &mappings)).collect();
+    println!("{:?}", keys);
+    
+    let keys = keys.iter().fold(1, |acc, x| integer::lcm(*x, acc));
+    println!("{:?}", keys);
+
+
+}
 
 fn main() {
     let contents = std::fs::read_to_string("input.txt").expect("Could not read file!");
@@ -77,13 +120,6 @@ fn main() {
         .map(|x| (x[0], (x[1], x[2])))
         .collect::<HashMap<_, _>>();
 
-    for line in &mappings {
-        println!("{:?}", line);
-    }
-
     // part1(turns, mappings);
-    part2(turns, mappings);
-
-
-
+    part2b(turns, mappings);
 }
